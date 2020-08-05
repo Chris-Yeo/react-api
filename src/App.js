95a702b4bb1ebdbcbfeddeb6f27ea73f82209280
Chris-Yeo <chrisyeo.tr@gmail.com>
2020-08-05 15:38:14 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import axios from 'axios';
 import './App.css';
 
 const App = () => {
-  const [news, setNews] = useState ([]);
+  const [giphy, setGiphy] = useState ([]);
   const [loading, setLoading] = useState (true);
   const [error, setError] = useState (false);
   const [errorMessage, setErrorMessage] = useState ();
 
   useEffect (() => {
-    const url='https://newsapi.org/v2/top-headlines?pageSize=9&sources=bbc-news&apiKey=ed2eab71ff4c44ed8fab3ef43f305400';
+    const url='https://api.giphy.com/v1/gifs/search?q=food&api_key=Vls5MyeXXm31FecUyfEfuLCaEPMF287H&limit=9';
     axios.get(url)
     .then(function(result) {
       console.log(result)
-     setNews(result.data.articles);
+     setGiphy(result.data.data);
       setLoading(false)
     })
     .catch(function(error) {
@@ -35,13 +35,12 @@ const App = () => {
           error ? (
             <div>{errorMessage}</div>
           ) : (
-              news.map((item) => (
+              giphy.map((item) => (
               
                   <div className="whole">
-                    <div className="image"><img src={item.urlToImage} alt="news" /></div>
+                    <div className="image"><img src={item.images.original.url} alt="giphy" /></div>
                     <div className="title"> <h3>{item.title}</h3></div>
-                    <div className="description"><p>{item.description}</p></div>
-                    <a href={item.url} className="link"><i className="fas fa-eye"><span>&nbsp; Read Article</span></i></a>
+                    <div className="description"><p>{item.type}</p></div>
                   </div>
                 
             ))
